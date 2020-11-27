1606b009ae6759b3b1ab136569157bd632e68ba9
bernat <bernatespinas@gmail.com>
2020-11-27 09:24:59 +0100

    Métodos para añadir y eliminar bebidas de un restaurante

--- backend/controller/RestauranteBebidaController.java
+++ backend/controller/RestauranteBebidaController.java
@@ -1,6 +1,7 @@
 package backend.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import backend.DBConn;
 
@@ -13,6 +14,16 @@ public class RestauranteBebidaController {
         "SELECT %s FROM %s WHERE %s = ?", ID_BEBIDA, TABLE, ID_RESTAURANTE
     );
 
+    private static final String INSERT = String.format(
+        "INSERT INTO %s (%s, %s) VALUES (?, ?)",
+        TABLE, ID_RESTAURANTE, ID_BEBIDA
+    );
+
+    private static final String DELETE = String.format(
+        "DELETE FROM %s WHERE %s = ? AND %s = ?",
+        TABLE, ID_RESTAURANTE, ID_BEBIDA
+    );
+
     public static List<Integer> getByIdRestaurante(int idRestaurante) {
         return DBConn.executeQueryGetIntIds(
             SELECT_BY_ID_RESTAURANTE,
@@ -20,6 +31,26 @@ public class RestauranteBebidaController {
                 {1, idRestaurante}
             },
             ID_BEBIDA
+        );
+    }
+
+    public static Optional<Integer> add(int idRestaurante, int idBebida) {
+        return DBConn.executeInsert(
+            INSERT,
+            new Object[][] {
+                {1, idRestaurante},
+                {2, idBebida}
+            }
+        );
+    }
+
+    public static boolean remove(int idRestaurante, int idBebida) {
+        return DBConn.executeUpdateOrDelete(
+            DELETE,
+            new Object[][] {
+                {1, idRestaurante},
+                {2, idBebida}
+            }
         );
     }
 }
