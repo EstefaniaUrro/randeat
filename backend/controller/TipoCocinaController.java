c7a8b6a058380c3e705693fbe97851ab72309e2d
Ismael Muñoz <ismaelahumao@hotmail.com>
2020-11-27 11:40:51 +0100

    TipoCocinaController y UsuarioController Update

--- backend/controller/TipoCocinaController.java
+++ backend/controller/TipoCocinaController.java
@@ -1,7 +1,10 @@
 package backend.controller;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 import java.util.Optional;
 
@@ -46,4 +49,28 @@ public class TipoCocinaController implements FromResultSet<TipoCocina> {
             resultSet.getString(NOMBRE)
         );
     }
+    public static void save(TipoCocina tipoCocina) {
+        String sql;
+        if (tipoCocina.getIdTipoCocina()>0) {
+        sql = String.format("UPDATE %s set %s=? WHERE %s=%d",
+        TABLE, NOMBRE, ID_TIPO_COCINA, tipoCocina.getIdTipoCocina());
+        } else {
+        sql = String.format("INSERT INTO %s (%s) VALUES (?)",
+        TABLE, NOMBRE);
+        }
+        try (Connection conn = DBConn.getConn();
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+        Statement stmt = conn.createStatement()) {
+        pstmt.setString(1, tipoCocina.getNombre());
+        pstmt.executeUpdate();
+        if (tipoCocina.getIdTipoCocina()==0) {
+        ResultSet rs = stmt.executeQuery("select last_insert_id()");
+        if (rs.next()) {
+        tipoCocina.setIdTipoCocina(rs.getInt(1));
+        }
+        }
+        } catch (Exception e) {
+        System.out.println(e.getMessage());
+        }
+        }
 }
--- backend/controller/UsuarioController.java
+++ backend/controller/UsuarioController.java
@@ -31,6 +31,11 @@ public class UsuarioController implements FromResultSet<Usuario> {
         TABLE, CORREO_ELECTRONICO, CONTRASENA, TELEFONO, POBLACION, DIRECCION
     );
 
+    private static final String UPDATE_USUARIO = String.format(
+        "UPDATE %s SET %s=?, %s=?, %s=?, %s=?, %s=? WHERE %s=?",
+        TABLE, CORREO_ELECTRONICO, CONTRASENA, TELEFONO, POBLACION, DIRECCION, ID_USUARIO
+    );
+
     public static List<Usuario> getAll() {
         return DBConn.executeQueryIntoList(SELECT_ALL, new UsuarioController());
     }
@@ -56,6 +61,19 @@ public class UsuarioController implements FromResultSet<Usuario> {
                 {5, usuario.getDireccion()}
             }
         );
+    }
+
+    public static boolean update(Usuario usuario) {
+        return DBConn.executeUpdateOrDelete(
+            UPDATE_USUARIO,
+             new Object[][] {
+                 {1, usuario.getCorreoElectronico()},
+                 {2, usuario.getContrasena()},
+                 {3, usuario.getTelefono()},
+                 {4, usuario.getPoblacion()},
+                 {5, usuario.getDireccion()}
+             }
+        );     
     }
 
     @Override
