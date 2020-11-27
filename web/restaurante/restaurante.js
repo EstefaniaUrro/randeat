32f23d386f85dfc996868c99d5699b81404d6533
Estefania <urroestefania@gmail.com>
2020-11-27 15:11:37 +0100

    27-11 restaurante

--- web/restaurante/restaurante.js
+++ web/restaurante/restaurante.js
@@ -1,11 +1,10 @@
-document.addEventListener('DOMContentLoaded', function(event) {
+document.addEventListener('DOMContentLoaded', function (event) {
 
     cargarOpcionesPaquete();
     createDeliveryOptions();
-    createDrinkLists();
     getUlByIdAndApplyClasses("visibility-options");
-});
 
+});
 function selectLiOption(element) {
     element.classList.toggle("selected");
 }
@@ -20,59 +19,71 @@ function toggleVisibilityOption(element) {
     }
 }
 
-function createLiOption(name, description) {
+function createLiOptions(name, description) {
     let li = document.createElement("li");
     li.classList.add("list-group-item");
     li.classList.add("list-group-item-action");
-    li.addEventListener("click", function() {
-        javascript:selectLiOption(this);
-    });
 
     let nameSpan = document.createElement("span");
     nameSpan.innerHTML = `<b>${name}</b>`;
     li.appendChild(nameSpan);
-    
     li.appendChild(document.createElement("br"));
-
     let descriptionSpan = document.createElement("span");
     descriptionSpan.classList.add("option-description");
     descriptionSpan.innerHTML = description;
     li.appendChild(descriptionSpan);
+    li.addEventListener("click", function () {
+        javascript: selectLiOption(this);
+    });
 
     return li;
 }
+function createLiOption(name, description, precio) {
+    let li = document.createElement("li");
+    li.classList.add("list-group-item");
+    li.classList.add("list-group-item-action");
 
+    let nameSpan = document.createElement("span");
+    nameSpan.innerHTML = `<b>${name}</b>`;
+    li.appendChild(nameSpan);
+    li.appendChild(document.createElement("br"));
+    let descriptionSpan = document.createElement("span");
+    descriptionSpan.classList.add("option-description");
+    descriptionSpan.innerHTML = description;
+    li.appendChild(descriptionSpan);
+    let input = document.createElement("input");
+    input.classList.add("form-control");
+    input.classList.add("col-12");
+    input.classList.add("col-md-4");
+    input.type = 'number';
+    input.placeholder = "Precio";
+    input.innerHTML = precio;
+    li.appendChild(input);
+    input.addEventListener("click", function () {
+        javascript: selectLiOption(this);
+    });
+    li.addEventListener("click", function () {
+        javascript: selectLiOption(this);
+    });
+    return li;
+}
 function getUlByIdAndApplyClasses(id) {
     let ul = document.getElementById(id);
     ul.classList = "list-group col-12 col-md-8 col-lg-8 col-xl-8 mx-auto py-3";
     return ul;
 }
 
-function createFoodOptions() {
-    let foodOptions = [
-        ["Italiano", "Pasta, pizza..."],
-        ["Chino", "Fideos, sopitas..."],
-        ["Vegano", "Ensaladas, tofu (XD)"]
-    ];
-
-    let ul = getUlByIdAndApplyClasses("food-options");
-
-    foodOptions.forEach(function(item, index) {
-        ul.appendChild(createLiOption(item[0], item[1]));
-    });
-}
-
 function cargarOpcionesPaquete() {
     let packageOptions = [
-        ["Pequeño", "Uno o dos entrantes"],
-        ["Mediano", "Un plato principal"],
-        ["Grande", "Un entrante y un plato principal"]
+        ["Pequeño", "Uno o dos entrantes", " "],
+        ["Mediano", "Un plato principal", " "],
+        ["Grande", "Un entrante y un plato principal", " "]
     ];
-
     let ul = getUlByIdAndApplyClasses("package-options");
 
-    packageOptions.forEach(function(item, index) {
-        ul.appendChild(createLiOption(item[0], item[1]));
+    packageOptions.forEach(function (item, index) {
+        ul.appendChild(createLiOption(item[0], item[1], item[2]));
+
     });
 }
 
@@ -84,42 +95,9 @@ function createDeliveryOptions() {
 
     let ul = getUlByIdAndApplyClasses("delivery-options");
 
-    deliveryOptions.forEach(function(item, index) {
-        ul.appendChild(createLiOption(item[0], item[1]));
+    deliveryOptions.forEach(function (item, index) {
+        ul.appendChild(createLiOptions(item[0], item[1]));
     });
 }
 
-function drinkItemOnClick(drinkElement, fromListId, toListId) {
-    document.getElementById(fromListId).removeChild(drinkElement);
-    document.getElementById(toListId).appendChild(drinkElement);
 
-    drinkElement.onclick = function() {
-        drinkItemOnClick(this, toListId, fromListId);
-    };
-}
-
-function createDrinkLists() {
-    let drinksNotAddedList = ["Coca-Cola", "Fanta naranja", "Fanta limón", "Pepsi", "Orchata"];
-    let drinksAddedList = ["Agua", "Agua de coco", "Agüita fresquita", "Aguarrás"];
-
-    let drinksNotAdded = document.getElementById("drinks-not-added");
-    let drinksAdded = document.getElementById("drinks-added");
-
-    addDrinksToList(drinksNotAddedList, drinksNotAdded, drinksAdded.id);
-    addDrinksToList(drinksAddedList, drinksAdded, drinksNotAdded.id);
-}
-
-function addDrinksToList(drinksList, listElement, otherListId) {
-    drinksList.forEach(function(item, index) {
-        let li = document.createElement("li");
-        li.classList.add("list-group-item");
-        li.classList.add("list-group-item-action");
-        
-        li.innerHTML = item;
-        li.onclick = function() {
-            drinkItemOnClick(this, listElement.id, otherListId);
-        };
-
-        listElement.appendChild(li);
-    });
-}
