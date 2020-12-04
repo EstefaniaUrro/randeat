d93d9c38cc1abfe55a2201af6e887ae882a51dd7
Ismael Muñoz <ismaelahumao@hotmail.com>
2020-12-04 12:53:44 +0100

    Escoger tipo comida con Fetch

--- web/cliente/FetchTipoComida.js
+++ web/cliente/FetchTipoComida.js
@@ -7,12 +7,14 @@ fetch(url)
   for (let tipoCocinaId = 0; tipoCocinaId <8; ++tipoCocinaId) {
   let tipoCocina = document.getElementById("tipoCocina")
   tipoCocina.innerHTML += `
-  <div class="card col-6 col-sm-6 col-md-3"style="background-color: rgba(243, 243, 243, 0.616); border-radius: 10px;">
-    <div class="card-body text-center">
-      <a href="escogerpaquete.html">
-        <h5 class="card-tittle" style="color: rgb(255, 115, 0);">${data[tipoCocinaId].nombre}</h5>
-      </a>
-    </div>
+  <div class="col-6 col-sm-6 col-md-3">
+      <div class="card"style="background-color: rgba(243, 243, 243, 0.616); border-radius: 10px; margin-top: 10px;">
+          <div class="card-body text-center">
+              <a href="escogerpaquete.html">
+                <h5 class="card-tittle" style="color: rgb(255, 115, 0);">${data[tipoCocinaId].nombre}</h5>
+              </a>
+          </div>
+      </div>
   </div>
   `};
   console.log(data)
@@ -20,78 +22,98 @@ fetch(url)
 .catch(err => console.log(err));
 
 
-/*<div class="card">
-      <div class="text-center">
-        <h1 class="card-title" style="color : orange" ;>¿Qué te apetece comer?</h1>
-      </div>
-      <div class="row borde">
-        <div class="card col-6 col-sm-6 col-md-3"
-          style="background-color: rgba(243, 243, 243, 0.616); border-radius: 10px;">
-          <div class="card-body text-center">
-            <a href="escogerpaquete.html">
-              <h5 class="card-tittle" style="color: rgb(255, 115, 0);">Pollo</h5>
-            </a>
-          </div>
+/*<div class="container">
+        <div class="card">
+            <div class="text-center">
+                <h1 class="card-title" style="color : orange" ;>¿Qué te apetece comer?</h1>
+            </div>
+            <br>
+            <div class="row borde">
+                <div class="col-6 col-sm-6 col-md-3">
+                    <div class="card"
+                        style="background-color: rgba(243, 243, 243, 0.616); border-radius: 10px; margin-top: 10px;">
+                        <div class="card-body text-center">
+                            <a href="escogerpaquete.html">
+                                <h5 class="card-tittle" style="color: rgb(255, 115, 0);">Pollo</h5>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class=" col-6 col-sm-6 col-md-3">
+                    <div class="card"
+                        style="background-color: rgba(243, 243, 243, 0.616); border-radius: 10px; margin-top: 10px;">
+                        <div class="card-body text-center">
+                            <a href="#">
+                                <h5 class="card-tittle" style="color: rgb(255, 115, 0);">Pollo</h5>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-sm-6 col-md-3">
+                    <div class="card"
+                        style="background-color: rgba(243, 243, 243, 0.616); border-radius: 10px; margin-top: 10px;">
+                        <div class="card-body text-center">
+                            <a href="#">
+                                <h5 class="card-tittle" style="color: rgb(255, 115, 0);">Pollo</h5>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-sm-6 col-md-3">
+                    <div class="card"
+                        style="background-color: rgba(243, 243, 243, 0.616); border-radius: 10px; margin-top: 10px;">
+                        <div class="card-body text-center">
+                            <a href="#">
+                                <h5 class="card-tittle" style="color: rgb(255, 115, 0);">Pollo</h5>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row borde">
+                <div class="col-6 col-sm-6 col-md-3">
+                    <div class="card"
+                        style="background-color: rgba(243, 243, 243, 0.616); border-radius: 10px; margin-top: 10px;">
+                        <div class="card-body text-center">
+                            <a href="escogerpaquete.html">
+                                <h5 class="card-tittle" style="color: rgb(255, 115, 0);">Pollo</h5>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class=" col-6 col-sm-6 col-md-3">
+                    <div class="card"
+                        style="background-color: rgba(243, 243, 243, 0.616); border-radius: 10px; margin-top: 10px;">
+                        <div class="card-body text-center">
+                            <a href="#">
+                                <h5 class="card-tittle" style="color: rgb(255, 115, 0);">Pollo</h5>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-sm-6 col-md-3">
+                    <div class="card"
+                        style="background-color: rgba(243, 243, 243, 0.616); border-radius: 10px; margin-top: 10px;">
+                        <div class="card-body text-center">
+                            <a href="#">
+                                <h5 class="card-tittle" style="color: rgb(255, 115, 0);">Pollo</h5>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 col-sm-6 col-md-3">
+                    <div class="card"
+                        style="background-color: rgba(243, 243, 243, 0.616); border-radius: 10px; margin-top: 10px;">
+                        <div class="card-body text-center">
+                            <a href="#">
+                                <h5 class="card-tittle" style="color: rgb(255, 115, 0);">Pollo</h5>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <br>
         </div>
-        <div class="card col-6 col-sm-6 col-md-3"
-          style="background-color: rgba(243, 243, 243, 0.616); border-radius: 10px;">
-          <div class="card-body text-center">
-            <a href="#">
-              <h5 class="card-tittle" style="color: rgb(255, 115, 0);">Pollo</h5>
-            </a>
-          </div>
-        </div>
-        <div class="card col-6 col-sm-6 col-md-3"
-          style="background-color: rgba(243, 243, 243, 0.616); border-radius: 10px;">
-          <div class="card-body text-center">
-            <a href="#">
-              <h5 class="card-tittle" style="color: rgb(255, 115, 0);">Pollo</h5>
-            </a>
-          </div>
-        </div>
-        <div class="card col-6 col-sm-6 col-md-3"
-          style="background-color: rgba(243, 243, 243, 0.616); border-radius: 10px;">
-          <div class="card-body text-center">
-            <a href="#">
-              <h5 class="card-tittle" style="color: rgb(255, 115, 0);">Pollo</h5>
-            </a>
-          </div>
-        </div>
-      </div>
-      <div class="row borde">
-        <div class="card col-6 col-sm-6 col-md-3"
-          style="background-color: rgba(243, 243, 243, 0.616); border-radius: 10px;">
-          <div class=" card-body text-center">
-            <a href="#">
-              <h5 class="card-tittle" style="color: rgb(255, 115, 0);">Pollo</h5>
-            </a>
-          </div>
-        </div>
-        <div class="card col-6 col-sm-6 col-md-3"
-          style="background-color: rgba(243, 243, 243, 0.616); border-radius: 10px;">
-          <div class="card-body text-center">
-            <a href="#">
-              <h5 class="card-tittle" style="color: rgb(255, 115, 0);">Pollo</h5>
-            </a>
-          </div>
-        </div>
-        <div class="card col-6 col-sm-6 col-md-3"
-          style="background-color: rgba(243, 243, 243, 0.616); border-radius: 10px;">
-          <div class="card-body text-center">
-            <a href="#">
-              <h5 class="card-tittle" style="color: rgb(255, 115, 0);">Pollo</h5>
-            </a>
-          </div>
-        </div>
-        <div class="card col-6 col-sm-6 col-md-3"
-          style="background-color: rgba(243, 243, 243, 0.616); border-radius: 10px;">
-          <div class="card-body text-center">
-            <a href="#">
-              <h5 class="card-tittle" style="color: rgb(255, 115, 0);">Pollo</h5>
-            </a>
-          </div>
-        </div>
-      </div>
-      <br>
     </div>
     */
