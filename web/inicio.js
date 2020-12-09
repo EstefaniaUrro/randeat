d93c389ea8d4e6ffb8ffbdd33921b69a9161c2df
bernat <bernatespinas@gmail.com>
2020-12-09 17:37:01 +0100

    Login inicial

---
+++ web/inicio.js
@@ -0,0 +1,76 @@
+const validateUrl = "http://localhost:8080/usuario/getByCredentials";
+const restauranteUrl = "http://localhost:8080/restaurante/getByIdUsuario";
+const clienteUrl = "http://localhost:8080/cliente/getByIdUsuario";
+
+function performLogin() {
+	localStorage.clear();
+
+	let form = document.forms["login"];
+
+	let email = form["email"].value;
+	console.log("email: ", email);
+
+	let password = form["password"].value;
+	console.log("password: ", password);
+
+	if (email === "" || password === "") {
+		alert("Introduce todos los datos, por favor");
+	} else {
+		fetch(`${validateUrl}?email=${email}&password=${password}`)
+		.then(response => response.json())
+		.then(jsonUsuario => {
+			if (jsonUsuario === null) {
+				alert("Correo electrónico o contraseña incorrectos");
+			} else {
+				console.log("found usuario: ", jsonUsuario);
+				localStorage.setItem(
+					"usuario",
+					JSON.stringify(jsonUsuario)
+				);
+
+				console.log(localStorage);
+
+				alert("Usuario logueado correctamente, voy a ver si es un cliente o un restaurante");
+
+				// Mirar si es Cliente o Restaurante.
+				fetch(`${restauranteUrl}/${jsonUsuario.idUsuario}`)
+				.then(response => response.json())
+				.then(jsonRestaurante => {
+					if (jsonRestaurante === null) {
+						alert("no es un restaurante, voy a ver si es un cliente");
+
+						fetch(`${clienteUrl}/${jsonUsuario.idUsuario}`)
+						.then(response => response.json())
+						.then(jsonCliente => {
+							if (jsonCliente === null) {
+								alert("tampoco es un cliente? algo va mal");
+							} else {
+								alert("es un cliente!!!!! guardando datos");
+								localStorage.setItem(
+									"cliente",
+									JSON.stringify(jsonCliente)
+								);
+							}
+						})
+						.catch(err => {
+							console.log("Error en fetch de cliente: ", err);
+						});
+					} else {
+						alert("es un restaurante!!! guardando datos");
+
+						localStorage.setItem(
+							"restaurante",
+							JSON.stringify(jsonRestaurante)
+						);
+					}
+				})
+				.catch(err => {
+					console.log("Error en fetch de restaurante: ", err)
+				});
+			}
+		})
+		.catch(err => {
+			console.log("Error en fetch de usuario: ", err);
+		});
+	}
+}
