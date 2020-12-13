2b67e1c7bf1b5e519db94c38891ee4c5b770947f
bernat <bernatespinas@gmail.com>
2020-12-13 13:34:46 +0100

    Index pasa info a escogertipocomida

--- web/cliente/FetchTipoComida.js
+++ web/cliente/FetchTipoComida.js
@@ -1,120 +1,41 @@
+function chooseTipoCocina(idTipoCocina) {
+	let infoPedido = JSON.parse(localStorage.getItem("infoPedido"));
+	infoPedido.idTipoCocina = idTipoCocina;
 
-const url = 'http://localhost:8080/tipoCocina/getAll'
+	localStorage.setItem("infoPedido", JSON.stringify(infoPedido));
 
-//crea las diferentes tarjetas de tipoCocina en el HTML
-fetch(url)
-.then(response => response.json())
-.then(data => {
-  for (let tipoCocinaId = 0; tipoCocinaId <8; ++tipoCocinaId) {
-  let tipoCocina = document.getElementById("tipoCocina")
-  tipoCocina.innerHTML += `
-  <div class="col-6 col-sm-6 col-md-3">
-      <div class="card"style="background-color: rgba(243, 243, 243, 0.616); border-radius: 10px; margin-top: 10px;">
-          <div class="card-body text-center">
-              <a href="escogerpaquete.html">
-                <h5 class="card-tittle" style="color: rgb(255, 115, 0);">${data[tipoCocinaId].nombre}</h5>
-              </a>
-          </div>
-      </div>
-  </div>
-  `};
-  console.log(data)
-})
-.catch(err => console.log(err));
+	window.location.href = "./cliente/escogerpaquete.html";
+}
 
+function fetchTipoCocinaFilter(idCodigoPostal, idTipoEntrega) {
+    const url = "http://localhost:8080/tipoCocina/getInFilter?"
+        + `idCodigoPostal=${idCodigoPostal}`
+        + `&idTipoEntrega=${idTipoEntrega}`    
+    ;
+    
+    //crea las diferentes tarjetas de tipoCocina en el HTML
+    fetch(url)
+        .then(response => response.json())
+        .then(json => {
+            let tipoCocinaDiv = document.getElementById("tipoCocina")
 
-/*<div class="container">
-        <div class="card">
-            <div class="text-center">
-                <h1 class="card-title" style="color : orange" ;>¿Qué te apetece comer?</h1>
-            </div>
-            <br>
-            <div class="row borde">
-                <div class="col-6 col-sm-6 col-md-3">
-                    <div class="card"
-                        style="background-color: rgba(243, 243, 243, 0.616); border-radius: 10px; margin-top: 10px;">
+            json.map(tipoCocina => {
+                tipoCocinaDiv.innerHTML += `
+                <div class="col-6 col-sm-6 col-md-3" onclick="chooseTipoCocina(${tipoCocina.idTipoCocina});">
+                    <div class="btn card" style="background-color: rgba(243, 243, 243, 0.616); border-radius: 10px; margin-top: 10px;">
                         <div class="card-body text-center">
-                            <a href="escogerpaquete.html">
-                                <h5 class="card-tittle" style="color: rgb(255, 115, 0);">Pollo</h5>
-                            </a>
+                            <h5 class="card-tittle" style="color: rgb(255, 115, 0);">${tipoCocina.nombre}</h5>
                         </div>
                     </div>
                 </div>
-                <div class=" col-6 col-sm-6 col-md-3">
-                    <div class="card"
-                        style="background-color: rgba(243, 243, 243, 0.616); border-radius: 10px; margin-top: 10px;">
-                        <div class="card-body text-center">
-                            <a href="#">
-                                <h5 class="card-tittle" style="color: rgb(255, 115, 0);">Pollo</h5>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-6 col-sm-6 col-md-3">
-                    <div class="card"
-                        style="background-color: rgba(243, 243, 243, 0.616); border-radius: 10px; margin-top: 10px;">
-                        <div class="card-body text-center">
-                            <a href="#">
-                                <h5 class="card-tittle" style="color: rgb(255, 115, 0);">Pollo</h5>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-6 col-sm-6 col-md-3">
-                    <div class="card"
-                        style="background-color: rgba(243, 243, 243, 0.616); border-radius: 10px; margin-top: 10px;">
-                        <div class="card-body text-center">
-                            <a href="#">
-                                <h5 class="card-tittle" style="color: rgb(255, 115, 0);">Pollo</h5>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="row borde">
-                <div class="col-6 col-sm-6 col-md-3">
-                    <div class="card"
-                        style="background-color: rgba(243, 243, 243, 0.616); border-radius: 10px; margin-top: 10px;">
-                        <div class="card-body text-center">
-                            <a href="escogerpaquete.html">
-                                <h5 class="card-tittle" style="color: rgb(255, 115, 0);">Pollo</h5>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div class=" col-6 col-sm-6 col-md-3">
-                    <div class="card"
-                        style="background-color: rgba(243, 243, 243, 0.616); border-radius: 10px; margin-top: 10px;">
-                        <div class="card-body text-center">
-                            <a href="#">
-                                <h5 class="card-tittle" style="color: rgb(255, 115, 0);">Pollo</h5>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-6 col-sm-6 col-md-3">
-                    <div class="card"
-                        style="background-color: rgba(243, 243, 243, 0.616); border-radius: 10px; margin-top: 10px;">
-                        <div class="card-body text-center">
-                            <a href="#">
-                                <h5 class="card-tittle" style="color: rgb(255, 115, 0);">Pollo</h5>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-6 col-sm-6 col-md-3">
-                    <div class="card"
-                        style="background-color: rgba(243, 243, 243, 0.616); border-radius: 10px; margin-top: 10px;">
-                        <div class="card-body text-center">
-                            <a href="#">
-                                <h5 class="card-tittle" style="color: rgb(255, 115, 0);">Pollo</h5>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            `});
+        })
+        .catch(err => console.log(err))
+    ;
+}
 
-            <br>
-        </div>
-    </div>
-    */
+document.addEventListener('DOMContentLoaded', function (event) {
+    let infoPedido = JSON.parse(localStorage.getItem("infoPedido"));
+
+	fetchTipoCocinaFilter(infoPedido.idCodigoPostal, infoPedido.idTipoEntrega);
+});
--- web/js/index.js
+++ web/js/index.js
@@ -13,8 +13,18 @@ function chooseTipoCocina(idTipoEntrega) {
 		return;
 	}
 
-	localStorage.setItem("idCodigoPostal", idCodigoPostal);
-	localStorage.setItem("idTipoEntrega", idTipoEntrega);
+	/* Información para hacer un pedido que necesita escogertipocomida.html. */
+	localStorage.setItem("infoPedido", JSON.stringify({
+		"idCodigoPostal": idCodigoPostal,
+		"idTipoEntrega": idTipoEntrega
+	}));
 
 	window.location.href = "./cliente/escogertipocomida.html";
 }
+
+/* Al ir a la pantalla principal, eliminar cualquier información para hacer un pedido que pueda estar guardada. */
+localStorage.removeItem("infoPedido");
+
+document.addEventListener('DOMContentLoaded', function (event) {
+	fillSelectCodigoPostal(null);
+});
