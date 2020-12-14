d2ed9c630487b2dc4276bc592a491cf160d5d715
Estefania Urro <urroestefania@gmail.com>
2020-12-14 14:01:28 +0100

    14-12 pedidos

--- web/js/pedidos.js
+++ web/js/pedidos.js
@@ -1,3 +1,33 @@
+function loadRestaurante() {
+	let cliente = JSON.parse(localStorage.getItem("cliente"));
+;
+	fetch(
+		`http://localhost:8080/pedido/getByIdCliente/${cliente.idCliente}`
+	).then(
+		response => response.json()
+	).then(ok => {
+		console.log(ok);
+		fetch(
+			`http://localhost:8080/pedido/getById/${ok[0].idPedido}`
+		).then(
+			response => response.json()
+		).then(nombre => {
+			console.log(nombre.restauranteIdRestaurante);
+			fetch(
+				`http://localhost:8080/restaurante/getById/${nombre.restauranteIdRestaurante}`
+			).then(
+				response => response.json()
+			).then(restaurante => {
+				console.log(restaurante.nombreRestaurante);
+				$("#tbodyModalCliente").append(`<tr><td>` + restaurante.nombreRestaurante  + `</td><td>` + nombre.direccionEnvio + `</td>
+				<td>` + nombre.comentario + `</td></tr>`);
+				
+			});
+		});
+	});
+	
+}
+
 async function loadPaquetesMap() {
 	const response = await fetch(
 		"http://localhost:8080/paquete/getAll"
@@ -27,7 +57,7 @@ async function loadTipoCocinaMap() {
 
 function createIdElementMap(json, idAttributeName) {
 	let map = new Map();
-
+	console.log("LISTA", map);
 	json.map(element => {
 		map.set(
 			element[idAttributeName],
@@ -65,7 +95,7 @@ function setPedidoPaquetesString(idPedido, tdPaquete, paquetesMap) {
 
 async function loadPedidosJson() {
 	let url = "";
-
+	
 	let cliente = JSON.parse(localStorage.getItem("cliente"));
 	if (cliente !== null) {
 		url = `http://localhost:8080/pedido/getByIdCliente/${cliente.idCliente}`;
@@ -73,16 +103,18 @@ async function loadPedidosJson() {
 		let restaurante = JSON.parse(localStorage.getItem("restaurante"));
 		if (restaurante !== null) {
 			url = `http://localhost:8080/pedido/getByIdRestaurante/${restaurante.idRestaurante}`;
+		
 		} else {
 			alert("Error al determinal el tipo de usuario. Por favor, cierra sesión y vuelve a conectarte.");
 		}
 	}
-
 	console.log("loadPedidosJson url: ", url);
 
 	const response = await fetch(url);
 	return await response.json();
 }
+
+
 
 function loadPedidos() {
 	Promise.all([
@@ -95,16 +127,16 @@ function loadPedidos() {
 		let tipoEntregaMap = values[1];
 		let tipoCocinaMap = values[2];
 		let paquetesMap = values[3];
-
+	
 		let tbody = document.getElementsByTagName("tbody")[0];
-		console.log(document.getElementsByTagName("tbody")[0]);
 		for (let i = 0; i < pedidosJson.length;) {
 			let cliente = JSON.parse(localStorage.getItem("cliente"));
 			if (cliente !== null) {
-				$("#tbodyModalCliente").append(`<tr><td>` + pedidosJson[i].idPedido + `</td><td>` + pedidosJson[i].direccionEnvio + `</td>
-		<td>` + pedidosJson[i].comentario + `</td></tr>`);
+				/*$("#tbodyModalCliente").append(`<tr><td>` + pedidosJson[i].restauranteIdRestaurante  + `</td><td>` + pedidosJson[i].direccionEnvio + `</td>
+				<td>` + pedidosJson[i].comentario + `</td></tr>`);*/
 			}
 			let restaurante = JSON.parse(localStorage.getItem("restaurante"));
+			
 			if (restaurante !== null) {
 				$("#tbodyModalRestaurante").append(`<tr><td>` + pedidosJson[i].clienteIdCliente + `</td><td>` + pedidosJson[i].direccionEnvio + `</td>
 			<td>` + pedidosJson[i].comentario + `</td></tr>`);
@@ -117,9 +149,12 @@ function loadPedidos() {
 
 			let thIdPedido = document.createElement("th");
 			thIdPedido.setAttribute("scope", "row");
-			thIdPedido.appendChild(document.createTextNode(
+			let span = document.createElement("span");
+			span.id = pedido.idPedido;
+			span.appendChild(document.createTextNode(
 				pedido.idPedido
 			));
+			thIdPedido.appendChild(span);
 			tr.appendChild(thIdPedido);
 
 			let tdPaquete = document.createElement("td");
@@ -157,7 +192,9 @@ function loadPedidos() {
 
 			let tdInfo = document.createElement("button");
 			tdInfo.type = "button";
-			tdInfo.id = pedido.idPedido;
+			tdInfo.addEventListener("click", function (){
+				nombre(pedido.idPedido);
+			})
 			tdInfo.classList.add("btn-primary");
 			tdInfo.setAttribute("data-toggle", "modal");
 			let cliente = JSON.parse(localStorage.getItem("cliente"));
@@ -176,7 +213,11 @@ function loadPedidos() {
 		});
 	});
 }
+function nombre(pedido){
+	alert("HOLA"+ pedido);
+}
 
 document.addEventListener('DOMContentLoaded', function (event) {
 	loadPedidos();
+	loadRestaurante();
 });
